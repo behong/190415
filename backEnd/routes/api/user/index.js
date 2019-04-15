b9337e665d6fd5c5574c31535d66397339c397d1
juf <sijm@naver.com>
2019-04-16 00:45:34 +0900

    상수 상단 변경

--- backEnd/routes/api/user/index.js
+++ backEnd/routes/api/user/index.js
@@ -2,24 +2,34 @@ var express = require('express');
 var createError = require('http-errors');
 var router = express.Router();
 
-/* GET home page. */
+const us =[
+  {
+    name :'홍성인',
+    age : 39
+  },
+  {
+    name :'홍지유',
+    age : 8
+  },
+  {
+    name :'홍소유',
+    age : 5
+  }
+]
+
 router.get('/', function(req, res, next) {
-  const us =[
-    {
-      name :'홍성인',
-      age : 39
-    },
-    {
-      name :'홍지유',
-      age : 8
-    },
-    {
-      name :'홍소유',
-      age : 5
-    }
-  ]
   res.send({ users: us });
 });
+
+router.post('/', (req, res, next) =>{
+  res.send({success:true})
+})
+router.put('/') ,(req,res,next) =>{
+  res.send({success:true})
+}
+router.delete('/') ,(req,res,next) =>{
+  res.send({success:true})
+}
 
 router.all('*', function(req, res, next) {
   //res.send({ title: 'api 진입' });
